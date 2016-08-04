b7ccfb83e74933a9297faaabfa818edda007ec3d
Matt Bertrand <matt@epidemico.com>
2016-08-04 10:49:38 -0400

    Added a get_config() method for package, and a PLUGIN_CLASS_EXPORTS property for each module.

--- gaia_plugin_demo/another_fake_plugin.py
+++ gaia_plugin_demo/another_fake_plugin.py
@@ -1,3 +1,21 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+##############################################################################
+#  Copyright Kitware Inc. and Epidemico Inc.
+#
+#  Licensed under the Apache License, Version 2.0 ( the "License" );
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+##############################################################################
 from gaia.inputs import GaiaIO
 from gaia.gaia_process import GaiaProcess
 
@@ -25,3 +43,9 @@ class AnotherFakeProcess(GaiaProcess):
 
     def compute(self):
         print ("Compute something with AnotherFakeProcess")
+
+
+PLUGIN_CLASS_EXPORTS = [
+    AnotherFakeIO,
+    AnotherFakeProcess
+]
--- setup.py
+++ setup.py
@@ -1,3 +1,21 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+##############################################################################
+#  Copyright Kitware Inc. and Epidemico Inc.
+#
+#  Licensed under the Apache License, Version 2.0 ( the "License" );
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+##############################################################################
 from setuptools import setup, find_packages
 
 setup(
@@ -10,7 +28,7 @@ setup(
   include_package_data=True,
   entry_points={
     'gaia.plugins': [
-            "fake_plugin = gaia_plugin_demo.fake_plugin",
+            "a_fake_plugin = gaia_plugin_demo.a_fake_plugin",
             "another_fake_plugin = gaia_plugin_demo.another_fake_plugin"
         ]
   }
